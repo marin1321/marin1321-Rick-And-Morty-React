6e804f026339d01e3d178f81524342fdfbdfc9bf
Oscar Marin <marinmolinao@gmail.com>
2022-02-24 06:08:29 -0500

    Firsta

--- src/components/Layout/Main/Main.jsx
+++ src/components/Layout/Main/Main.jsx
@@ -2,20 +2,22 @@ import React,{useState} from 'react'
 
 export const Main = () => {
 
-    let character = []
-
-    const sendCharacters = (data) => {
-        console.log(data)
-        character=data
-        console.log(character);
-    }
+    const [characters, setCharacters] = useState([
+        {name: 'Rick Sanchez',
+        image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        species: "Human"}
+    ])
+    console.log(characters);
 
     const captureCharacters = () =>{
         const nombre = document.getElementById('nombre')
+        console.log(nombre.value);
         const url = 'https://rickandmortyapi.com/api/character/?name='+nombre.value
+        console.log(url);
         fetch(url)
         .then(response => response.json())
-        .then(data => sendCharacters(data.results));
+        .then(data => setCharacters(data.results));
+        
     }
 
     const validateEvent = (e) =>{
@@ -26,11 +28,22 @@ export const Main = () => {
 
   return (
     <div>
-        <input type="text" id='nombre' onKeyPress={validateEvent} />
-        {character.map((a)=>(
-            console.log(a)
-        ))}
-        
+        <div className="Search">
+        <input type="text" id='nombre' onKeyPress={validateEvent} placeholder="Buscar Nombre" />
+        </div>
+        <div className='cards'>
+        {characters.map((index,key) =>{
+            return(
+                <div key={key} className='card'>
+                    <img src={index.image} alt="" className='imgCard' />
+                    <div className='infoCard'>
+                    <p>{index.name}</p>
+                    <p>{index.species}</p>
+                    </div>
+                </div>
+            )
+        })}
+        </div>
         
     </div>
   )
